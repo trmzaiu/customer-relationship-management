a9ae6bff4f5809d4b53c662efb4e6c310c3683e2
tunsimp <nguyenquoctuanwar@gmail.com>
2025-04-28 16:46:01 +0700

    Using Atlas

--- backend/db/mongo.py
+++ backend/db/mongo.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017')
+client = MongoClient("mongodb+srv://ismproject:<db_password>@ismproject.iao7sef.mongodb.net/?retryWrites=true&w=majority&appName=ISMProject")
 db = client.mydatabase
